e472400e14104ac604830f58287f6e9c0ffce9aa
unknown <walid67ouarrad@gmail.com>
2023-11-30 21:19:35 -0500

    connecter à la base de donnée

---
+++ header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Mon Menu</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <nav class="navbar sticky-top navbar-light bg-light">
+
+    <style>
+       
+        body {
+            margin: 0;
+            font-family: Arial, sans-serif;
+        }
+
+        .navbar {
+            background-color: #f8f9fa;
+            overflow: hidden;
+        }
+        .navbar a {
+            float: left;
+            display: block;
+            color: #333;
+            text-align: center;
+            padding: 14px 80px;
+            text-decoration: none;
+        }
+
+        .navbar a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        .navbar a.active {
+            background-color: #4CAF50;
+            color: white;
+        }
+
+        .navbar a:hover,
+        .dropdown:hover .dropbtn {
+            background-color: #ddd;
+            color: black;
+        }
+
+        
+    </style>
+    </nav>
+</head>
+<body>
+
+<nav class="navbar">
+    <a class="active" href="index.php"><i class="fas fa-home"></i> Home</a>
+    <a href="login.php"><i class="fas fa-user"></i>login </a>
+   
+        </div>
+    </div>
+</nav>
+</body>
+</html>
