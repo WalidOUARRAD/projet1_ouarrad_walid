0d7d67628740cc651a96e8c82aaa87f10a538f69
unknown <walid67ouarrad@gmail.com>
2023-12-19 21:39:10 -0500

    creation de methode de recuperation des donnees du formulaire de l'ajout du produits

--- ajoute.php
+++ ajoute.php
@@ -1,3 +1,22 @@
+<?php
+include('../admin/header.php');
+include('fonction.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['action'])) {
+        $action = $_POST['action'];
+
+        switch ($action) {
+            case 'ajouter':
+                ajouterProduit($_POST);
+                break;
+        }
+    }
+}
+
+
+?>
+
 
 <!DOCTYPE html>
 <html lang="fr">
