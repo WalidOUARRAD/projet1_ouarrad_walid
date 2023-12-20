9611e0f11b1bffa813e181ae4886c6bd715bbe6c
unknown <walid67ouarrad@gmail.com>
2023-12-19 22:02:53 -0500

    creation du code html de la page responsable d'affichage des produits

---
+++ home.php
@@ -0,0 +1,49 @@
+
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    
+</head>
+
+<body>
+    <button onclick="window.location.href='panier.php';">Mon Panier</button>
+
+    <div class="products-container">
+        <form method="post" action="" enctype="multipart/form-data">
+            <label for="imageFile">Image:</label>
+            <input type="file" name="imageFile"><br>
+            <input type="submit" value="Télécharger">
+        </form>
+
+        
+            <div class="product-item">
+                <div class="product-image">
+                    <img src="" alt="">
+                </div>
+                <h3></h3>
+                <p>Prix: </p>
+                <p>Description: </p>
+                <form method="post" action="panier.php?action=add&pid=">
+                    <input type="text" class="product-quantity" name="quantity" value="1" size="2" />
+                    <input type="hidden" name="id_produit" value="">
+                    <label for="taille">Taille :</label>
+                    <select name="taille" id="taille">
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                    </select>
+                    <input type="submit" value="Ajouter au panier" class="btnAddAction" />
+                </form>
+            </div>
+        
+            <p>Aucun produit trouvé.</p>
+       
+    </div>
+</body>
+
+</html>
