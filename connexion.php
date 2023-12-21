0ac6f8b2763f455b16611d6ed9f3c4dbfc689277
unknown <walid67ouarrad@gmail.com>
2023-12-20 19:36:32 -0500

    rectification du nom de la DB

--- connexion.php
+++ connexion.php
@@ -2,7 +2,7 @@
 $server = 'localhost';
 $userName = "root";
 $pwd = "";
-$db = "ecom1_project";
+$db = "ecom1_projet";
 
 $conn = mysqli_connect($server, $userName, $pwd, $db);
 if ($conn) {
@@ -12,7 +12,7 @@ if ($conn) {
     echo "Error : Not connected to the $db database";
 }
 function connexionDb() {
-    $conn = mysqli_connect("localhost", "root", "", "ecom1_project");
+    $conn = mysqli_connect("localhost", "root", "", "ecom1_projet");
     if (mysqli_connect_errno()) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
         exit();
