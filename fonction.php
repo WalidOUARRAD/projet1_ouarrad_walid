0c64d4833ba9721879e803684b6b75514998e40f
unknown <walid67ouarrad@gmail.com>
2023-12-20 19:04:41 -0500

    function pour ajouter un produit

--- fonction.php
+++ fonction.php
@@ -76,4 +76,49 @@ function registerUser($user_name, $email, $password)
     }
 }
 
+function ajouterProduit($data) {
+    $conn = connexionDb();
+
+    $Nom = $data['newName'] ?? '';
+    $Quantite = $data['newQuantite'] ?? '';
+    $Prix = $data['newPrix'] ?? '';
+    $Image = $_FILES['newImage']['name'] ?? '';
+    $Description = $data['newDescription'] ?? '';
+
+    if (!empty($Nom) && !empty($Quantite) && !empty($Prix) && !empty($Image)) {
+        $Date_ajout = date('Y-m-d');
+
+        
+        $targetDir = "C:\Users\DELL\Downloads\image"; //a rectifier
+
+       
+        $targetFile = $targetDir . basename($Image);
+
+        
+        if (move_uploaded_file($_FILES["newImage"]["tmp_name"], $targetFile)) {
+           
+            $sql = "INSERT INTO product(name, quantity, price, img_url, description) VALUES (?, ?, ?, ?, ?)";
+            $stmt = mysqli_prepare($conn, $sql);
+            mysqli_stmt_bind_param($stmt, "sdsds", $Nom, $Quantite, $Prix, $Image, $Description);
+
+            
+            $resultat = mysqli_stmt_execute($stmt);
+
+            if ($resultat) {
+                echo "Ajout réussi";
+            } else {
+                echo "Une erreur est survenue lors de l'ajout : " . mysqli_error($conn);
+            }
+
+            mysqli_stmt_close($stmt);
+        } else {
+            echo 'Erreur lors du téléchargement du fichier.';
+        }
+
+        mysqli_close($conn);
+    } else {
+        echo "Remplissez tous les champs s'il vous plaît!";
+    }
+}
+
 ?>
