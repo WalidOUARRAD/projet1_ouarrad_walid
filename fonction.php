fde1be2e552e7f1468d312ef657543fbef71639f
unknown <walid67ouarrad@gmail.com>
2023-12-20 19:06:23 -0500

    function pour obtenir le produit de la base de données

--- fonction.php
+++ fonction.php
@@ -121,4 +121,20 @@ function ajouterProduit($data) {
     }
 }
 
+function obtenirProduitsDeLaBaseDeDonnees() {
+    $conn = connexionDb();
+
+    $sql = "SELECT * FROM product";
+    $result = $conn->query($sql);
+
+    $products = [];
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $products[] = $row;
+        }
+    }
+
+    $conn->close();
+    return $products;
+}
 ?>
